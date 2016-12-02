6bc7fe3f5601b81d2d616e7163ecacbed982d70f
Alexandre Dumont <alexandre@sogilis.com>
2016-12-02 10:48:45 +0100

    :sparkles: implement API call to scale a project database

---
+++ squarescale/scale-db.go
@@ -0,0 +1,40 @@
+package squarescale
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// ScaleDB calls the Squarescale API to update database scale options for a given project.
+func ScaleDB(sqscURL, token, project, instance string) error {
+	var payload struct {
+		Project struct {
+			InstanceClass string `json:"db_instance_class"`
+		} `json:"project"`
+	}
+
+	payload.Project.InstanceClass = "db.t2." + instance
+	payloadBytes, err := json.Marshal(&payload)
+	if err != nil {
+		return err
+	}
+
+	req := SqscRequest{
+		Method: "POST",
+		URL:    sqscURL + "/projects/" + project + "/cluster",
+		Token:  token,
+		Body:   payloadBytes,
+	}
+
+	res, err := doRequest(req)
+	if err != nil {
+		return err
+	}
+
+	if res.Code != http.StatusNoContent {
+		return fmt.Errorf("'%s %s' return code: %d", req.Method, req.URL, res.Code)
+	}
+
+	return nil
+}
