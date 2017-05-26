cc3011c13fae16371e7374c27f6ddee3b6b5e068
Yves Brissaud <eunomie@squarescale.com>
2017-05-26 11:24:01 +0200

    Add structured log with log level based on LOG_LEVEL env var

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "os"
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
+	log.SetOutput(os.Stdout)
+
+	logLevel := os.Getenv("LOG_LEVEL")
+	switch logLevel {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	default:
+		log.SetLevel(log.WarnLevel)
+	}
+
 	os.Exit(Run(os.Args[1:]))
 }
