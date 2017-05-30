6573a1c8ebc1b497c963ca5158f304a3f2a5b2f1
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2017-05-30 18:24:59 +0200

    Show endpoint in status command

--- command/status.go
+++ command/status.go
@@ -26,6 +26,7 @@ func (c *StatusCommand) Run(args []string) int {
 		return c.errorWithUsage(fmt.Errorf("Unparsed arguments on the command line: %v", c.flagSet.Args()))
 	}
 
+	fmt.Printf("Squarescale endpoint: %s\n", endpoint.String())
 	return c.runWithSpinner("check authorization", endpoint.String(), func(client *squarescale.Client) (string, error) {
 		return "You're currently logged in", nil // do nothing as we are already authenticated here.
 	})
