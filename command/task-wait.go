1dc89c3f8d82f8211a67a4187b208b806b2dbdc7
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2017-09-05 12:34:16 +0200

    [290] Fix :bug: parsing tasks JSON
    Some times were nil and we couldn't unmarshal them to times directly. This
    commit reverts the old type to strings and add time parsing as necessary.
    
    https://github.com/squarescale/platform/issues/290

--- command/task-wait.go
+++ command/task-wait.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/squarescale/squarescale-cli/squarescale"
 )
@@ -47,9 +46,9 @@ func (c *TaskWaitCommand) Run(args []string) int {
 			[]string{"Status", t.Status},
 			[]string{"Params", string(t.Params)},
 			[]string{"Completed By", t.CompletedBy},
-			[]string{"Created At", t.CreatedAt.Format(time.RFC3339)},
-			[]string{"Updated At", t.UpdatedAt.Format(time.RFC3339)},
-			[]string{"Completed At", t.CompletedAt.Format(time.RFC3339)},
+			[]string{"Created At", t.CreatedAt},
+			[]string{"Updated At", t.UpdatedAt},
+			[]string{"Completed At", t.CompletedAt},
 			[]string{"Waiting Events", strconv.Itoa(len(t.WaitingEvents))},
 		}
 		for _, ev := range t.WaitingEvents {
