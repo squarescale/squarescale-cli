4a3f68ac52f5f75ea563ff8fcfae24b47b426f8c
nicoudsarah <sarah@sogilis.com>
2020-04-08 11:19:13 +0200

    [DEV] batch-test.go: add tests to each element of JSON response

--- squarescale/batch_test.go
+++ squarescale/batch_test.go
@@ -21,7 +21,7 @@ func TestGetBatches(t *testing.T) {
 func nominalCase(t *testing.T) {
 
 	/*
-		1 completer la payload et la vérifier dans le then (regarder exemples sur infrastructure-scheduler)
+		1 OK - completer la payload et la vérifier dans le then (regarder exemples sur infrastructure-scheduler)
 		2 vérifier dans le "mock" http que l'url soit correcte
 		3 vérifier que l'on ai bien le token passé au bon endroit
 		4 tester Not Found (à faire dans un autre cas de tests)
@@ -35,19 +35,6 @@ func nominalCase(t *testing.T) {
 	projectName := "titi"
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		/*
-			{
-
-				"docker_image" => {"name" => self.container.docker_image.name},
-				"refresh_url" => self.container.container_callbacks.map { |cb| cb.full_url_path(:refresh) }[0],
-				"volumes" => self.mounted_volumes
-			}
-
-			DEFAULT_LIMITS = {
-
-			}.freeze
-		*/
 		resBody := `
 			[
 				{
@@ -65,7 +52,7 @@ func nominalCase(t *testing.T) {
 					"default_environment": "basic-environment",
 					"run_command": ["command1"],
 					"status": {
-						"infra": "ok",
+						"display_infra_status": "ok",
 						"schedule": {
 							"running": 0,
 							"instances": 0,
@@ -103,4 +90,89 @@ func nominalCase(t *testing.T) {
 	if len(batches) != 1 {
 		t.Fatalf("Expect %d, got %d", 1, len(batches))
 	}
+
+	if batches[0].Name != "my-little-batch" {
+		t.Errorf("Expect batchName %s , got %s", "my-little-batch", batches[0].Name)
+	}
+
+	if batches[0].Periodic != true {
+		t.Errorf("Expect batchPeriodic %s , got %t", "true", batches[0].Periodic)
+	}
+
+	if batches[0].CronExpression != "* * * * *" {
+		t.Errorf("Expect batchCronExpression %s , got %s", "* * * * *", batches[0].CronExpression)
+	}
+
+	if batches[0].TimeZoneName != "Europe/Paris" {
+		t.Errorf("Expect batchTimeZoneName %s , got %s", "Europe/Paris", batches[0].TimeZoneName)
+	}
+
+	if batches[0].Limits.Memory != 256 {
+		t.Errorf("Expect batchesLimitsMemory %d , got %d", 256, batches[0].Limits.Memory)
+	}
+
+	if batches[0].Limits.CPU != 100 {
+		t.Errorf("Expect batchesLimitsCPU %d , got %d", 100, batches[0].Limits.CPU)
+	}
+
+	if batches[0].Limits.NET != 1 {
+		t.Errorf("Expect batchesLimitsNet %d , got %d", 1, batches[0].Limits.NET)
+	}
+
+	if batches[0].Limits.IOPS != 0 {
+		t.Errorf("Expect batchesLimitsIops %d , got %d", 0, batches[0].Limits.IOPS)
+	}
+
+	if batches[0].CustomEnvironment != "final-environment" {
+		t.Errorf("Expect batchesCustomEnvironment %s , got %s", "final-environment", batches[0].CustomEnvironment)
+	}
+
+	if batches[0].DefaultEnvironment != "basic-environment" {
+		t.Errorf("Expect batchesDefaultEnvironment %s , got %s", "basic-environment", batches[0].DefaultEnvironment)
+	}
+
+	if len(batches[0].RunCommand) != 1 {
+		t.Errorf("Expect batchesRunCommand length of %d , got %d", 1, len(batches[0].RunCommand))
+	}
+
+	if batches[0].RunCommand[0] != "command1" {
+		t.Errorf("Expect batchesRunCommand %s , got %d", "command1", len(batches[0].RunCommand))
+	}
+
+	if batches[0].Status.Infra != "ok" {
+		t.Errorf("Expect batchesStatusInfrastructures %s , got %s", "ok", batches[0].Status.Infra)
+	}
+
+	if batches[0].Status.Schedule.Running != 0 {
+		t.Errorf("Expect batchesStatusRunning %d , got %d", 0, batches[0].Status.Schedule.Running)
+	}
+
+	if batches[0].Status.Schedule.Instances != 0 {
+		t.Errorf("Expect batchesStatusInstances %d , got %d", 0, batches[0].Status.Schedule.Instances)
+	}
+
+	if batches[0].Status.Schedule.Level != "ok" {
+		t.Errorf("Expect batchesStatusLevel %s , got %s", "ok", batches[0].Status.Schedule.Level)
+	}
+
+	if batches[0].Status.Schedule.Message != "Scheduled" {
+		t.Errorf("Expect batchesStatusMessage %s , got %s", "Scheduled", batches[0].Status.Schedule.Message)
+	}
+
+	if batches[0].DockerImage.Name != "my-little-image" {
+		t.Errorf("Expect batchesDockerImageName %s , got %s", "my-little-image", batches[0].DockerImage.Name)
+	}
+
+	if len(batches[0].RefreshUrl) != 1 {
+		t.Errorf("Expect batchesRefreshUrl length of %d , got %d", 1, len(batches[0].RefreshUrl))
+	}
+
+	if batches[0].RefreshUrl[0] != "new-url" {
+		t.Errorf("Expect batchesRefreshUrl %s , got %s", "new-url", batches[0].RefreshUrl[0])
+	}
+
+	if batches[0].Volumes != "volume1" {
+		t.Errorf("Expect batchesRefreshUrl %s , got %s", "volume1", batches[0].Volumes)
+	}
+
 }
