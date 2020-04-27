8607d5f02fbee109345b2aeb783fee54fbbbf6e2
nicoudsarah <sarah@sogilis.com>
2020-04-27 15:38:06 +0200

    [DEV] batch.go: add batch.go cli file on commands and rename batch.go file to batches.go on squarescale to make the difference

---
+++ command/batch.go
@@ -0,0 +1,31 @@
+package command
+
+import (
+	"strings"
+
+	"github.com/mitchellh/cli"
+)
+
+// BatchCommand is a cli.Command implementation for top level `sqsc batch` command.
+type BatchCommand struct {
+}
+
+// Run is part of cli.Command implementation.
+func (c *BatchCommand) Run(args []string) int {
+	return cli.RunResultHelp
+}
+
+// Synopsis is part of cli.Command implementation.
+func (c *BatchCommand) Synopsis() string {
+	return "Commands related to a batch"
+}
+
+// Help is part of cli.Command implementation.
+func (c *BatchCommand) Help() string {
+	helpText := `
+usage: sqsc batch <subcommand>
+  Run a project batch related command.
+  List of supported subcommands is available below.
+`
+	return strings.TrimSpace(helpText)
+}
