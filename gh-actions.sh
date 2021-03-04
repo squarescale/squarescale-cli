53e080de554a30688e79e2626cda09bea43c5375
Benoit DUMAS <benoit@sogilis.com>
2021-03-04 16:11:49 +0100

    Edit gh-actions.sh

--- gh-actions.sh
+++ gh-actions.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-if [ -z "$ORGANIZATION" ]; then
-  echo "ORGANIZATION is not set. Quitting."
+if [ -z "$ORGANIZATION_NAME" ]; then
+  echo "ORGANIZATION_NAME is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$PROJECT" ]; then
-  echo "PROJECT is not set. Quitting."
+if [ -z "$PROJECT_NAME" ]; then
+  echo "PROJECT_NAME is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$SERVICE" ]; then
-  echo "SERVICE is not set. Quitting."
+if [ -z "$WEB_SERVICE_NAME" ]; then
+  echo "WEB_SERVICE_NAME is not set. Quitting."
   exit 1
 fi
 
@@ -58,24 +58,24 @@ if [ -z "$NODE_TYPE" ]; then
 fi
 
 echo "Create project if not exists"
-if ! /sqsc project get -project-name $ORGANIZATION}/$PROJECT; then
+if ! /sqsc project get -project-name $ORGANIZATION_NAME}/$PROJECT_NAME; then
   /sqsc project create \
     -credential $IAAS_CRED \
     -monitoring netdata \
-    -name $PROJECT \
+    -name $PROJECT_NAME \
     -node-size $NODE_TYPE \
     -infra-type high-availability \
-    -organization $ORGANIZATION \
+    -organization $ORGANIZATION_NAME \
     -provider $IAAS_PROVIDER \
     -region $IAAS_REGION \
     -yes
 fi
 
 echo "Create service if not exists"
-if ! /sqsc container list --project-name $ORGANIZATION}/$PROJECT | grep $SERVICE; then
+if ! /sqsc container list --project-name $ORGANIZATION_NAME}/$PROJECT_NAME | grep $WEB_SERVICE_NAME; then
   /sqsc container add \
-    -project-name $ORGANIZATION/$PROJECT \
-    -servicename $SERVICE \
+    -project-name $ORGANIZATION_NAME/$PROJECT_NAME \
+    -servicename $WEB_SERVICE_NAME \
     -name $DOCKER_REPOSITORY:$DOCKER_REPOSITORY_TAG \
     -username $DOCKER_USER \
     -password $DOCKER_TOKEN
@@ -83,16 +83,16 @@ fi
 
 echo "Open HTTP port"
 NETWORK_RULES_NAME=http
-if ! /sqsc network-rule list -project-name $ORGANIZATION/$PROJECT -service-name $SERVICE | grep $NETWORK_RULES_NAME; then
+if ! /sqsc network-rule list -project-name $ORGANIZATION_NAME/$PROJECT_NAME -service-name $WEB_SERVICE_NAME | grep $NETWORK_RULES_NAME; then
   /sqsc \
     network-rule create \
-    -project-name $ORGANIZATION/$PROJECT \
+    -project-name $ORGANIZATION_NAME/$PROJECT_NAME \
     -external-protocol http \
     -internal-port 80 \
     -internal-protocol http \
     -name $NETWORK_RULES_NAME \
-    -service-name $SERVICE
+    -service-name $WEB_SERVICE_NAME
 fi
 
 echo "Schedule web service"
-/sqsc service schedule --project-name $ORGANIZATION/$PROJECT $SERVICE
+/sqsc service schedule --project-name $ORGANIZATION_NAME/$PROJECT_NAME $WEB_SERVICE_NAME
