3a0550f1c4a670314cf61bb9ddc77c1f1e51640e
Benoit DUMAS <benoit@sogilis.com>
2021-03-12 14:34:33 +0100

    Remove printing json mapEnvVar

--- gh-actions/EnvVar.go
+++ gh-actions/EnvVar.go
@@ -29,10 +29,6 @@ func (ev *EnvVar) create() {
 			log.Fatal("Cannot write json file with map environment variables.")
 		}
 
-		fmt.Println("The json generated looks like:")
-		jsonOutput, _ := exec.Command("/bin/sh", "-c", fmt.Sprintf("cat %s", jsonFileName)).Output()
-		fmt.Println(string(jsonOutput))
-
 		instancesNumber := "1"
 
 		cmd := fmt.Sprintf(
