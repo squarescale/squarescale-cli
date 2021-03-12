9117e30f54f76ac64e4132802dcc5eee6b19f416
Benoit DUMAS <benoit@sogilis.com>
2021-03-12 09:28:34 +0100

    Add EnVar inserting after database creation

---
+++ gh-actions/EnvVar.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/hoisie/mustache"
+)
+
+type EnvVar struct{}
+
+func (ev *EnvVar) create() {
+	if _, exists := os.LookupEnv(mapEnvVar); exists {
+		fmt.Println("Inserting database environment variables...")
+
+		data := mustache.Render(os.Getenv(mapEnvVar), map[string]string{
+			"DB_HOST":     getSQSCEnvValue("DB_HOST"),
+			"DB_USERNAME": getSQSCEnvValue("DB_USERNAME"),
+			"DB_PASSWORD": getSQSCEnvValue("DB_PASSWORD"),
+		})
+
+		jsonFileName := "mapEnvVar.json"
+		jsonErr := ioutil.WriteFile(jsonFileName, []byte(data), os.ModePerm)
+
+		if jsonErr != nil {
+			log.Fatal("Cannot write json file with map environment variables.")
+		}
+
+		instancesNumber := "1"
+
+		cmd := fmt.Sprintf(
+			"/sqsc container set -project-name %s/%s -env %s -service %s -instances %s -command \"%s\"",
+			os.Getenv(organizationName),
+			os.Getenv(projectName),
+			jsonFileName,
+			os.Getenv(webServiceName),
+			instancesNumber,
+			getCmdEnvValue(),
+		)
+		fmt.Println(cmd)
+		output, cmdErr := exec.Command("/bin/sh", "-c", cmd).Output()
+		fmt.Println(string(output))
+
+		if cmdErr != nil {
+			fmt.Println(cmd)
+			log.Fatal("Fail to import database environment variables.")
+		}
+	}
+}
+
+func getSQSCEnvValue(key string) string {
+	value, err := exec.Command("/bin/sh", "-c", fmt.Sprintf(
+		"/sqsc env get -project-name %s/%s \"%s\" | grep -v %s | tr -d '\n'",
+		os.Getenv(organizationName),
+		os.Getenv(projectName),
+		key,
+		"...done",
+	)).Output()
+
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Environment variable %q does not exists in this project.", key))
+		return ""
+	}
+
+	return string(value)
+}
--- gh-actions/database.go
+++ gh-actions/database.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-
-	"github.com/hoisie/mustache"
 )
 
 type Database struct{}
@@ -23,8 +20,6 @@ func (d *Database) create() {
 		} else {
 			fmt.Println("Database already exists.")
 		}
-
-		insertDatabaseEnvironement()
 	} else {
 		fmt.Println(fmt.Sprintf("%s, %s, %s are not set. No database will be created.", dbEngine, dbEngineVersion, dbSize))
 	}
@@ -51,45 +46,6 @@ func createDatabase() {
 	}
 }
 
-func insertDatabaseEnvironement() {
-	if _, exists := os.LookupEnv(mapEnvVar); exists {
-		fmt.Println("Inserting database environment variables...")
-
-		data := mustache.Render(os.Getenv(mapEnvVar), map[string]string{
-			"DB_HOST":     getSQSCEnvValue("DB_HOST"),
-			"DB_USERNAME": getSQSCEnvValue("DB_USERNAME"),
-			"DB_PASSWORD": getSQSCEnvValue("DB_PASSWORD"),
-		})
-
-		jsonFileName := "mapEnvVar.json"
-		jsonErr := ioutil.WriteFile(jsonFileName, []byte(data), os.ModePerm)
-
-		if jsonErr != nil {
-			log.Fatal("Cannot write json file with map environment variables.")
-		}
-
-		instancesNumber := "1"
-
-		cmd := fmt.Sprintf(
-			"/sqsc container set -project-name %s/%s -env %s -service %s -instances %s -command \"%s\"",
-			os.Getenv(organizationName),
-			os.Getenv(projectName),
-			jsonFileName,
-			os.Getenv(webServiceName),
-			instancesNumber,
-			getCmdEnvValue(),
-		)
-		fmt.Println(cmd)
-		output, cmdErr := exec.Command("/bin/sh", "-c", cmd).Output()
-		fmt.Println(string(output))
-
-		if cmdErr != nil {
-			fmt.Println(cmd)
-			log.Fatal("Fail to import database environment variables.")
-		}
-	}
-}
-
 func isDabataseExists() bool {
 	_, databaseNotExists := exec.Command("/bin/sh", "-c", fmt.Sprintf(
 		"/sqsc db show -project-name %s/%s | grep \"DB enabled\" | grep true",
@@ -98,16 +54,4 @@ func isDabataseExists() bool {
 	)).Output()
 
 	return databaseNotExists == nil
-}
-
-func getSQSCEnvValue(key string) string {
-	value, _ := exec.Command("/bin/sh", "-c", fmt.Sprintf(
-		"/sqsc env get -project-name %s/%s \"%s\" | grep -v %s | tr -d '\n'",
-		os.Getenv(organizationName),
-		os.Getenv(projectName),
-		key,
-		"...done",
-	)).Output()
-
-	return string(value)
 }
